79c59da9c83feb060d6adc646d36d593cf14d725
Angel Martinez <angel.martinez3@utp.ac.pa>
2024-09-02 14:56:25 -0500

    Laboratorio2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespasce Laboratorio2
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        //Ejemplo utilizando las variables de instancia de clase.
+        client.FirtsName = "Angel";
+        client.LastName = "Martinez";
+        client.age = 29;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+}
