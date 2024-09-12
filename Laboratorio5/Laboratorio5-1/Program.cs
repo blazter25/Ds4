f52560ebf8ec9fcec1e41c0074a2645b797de368
Angel Martinez <angel.martinez3@utp.ac.pa>
2024-09-11 19:18:53 -0500

    Lab5 completado

--- Laboratorio5/Laboratorio5-1/Program.cs
+++ Laboratorio5/Laboratorio5-1/Program.cs
@@ -1,36 +1,43 @@
-﻿private int[,] mat;
+﻿using System;
 
-public void Ingresar()
-
+public class Matriz
 {
-    Mat = new int[3, 4];
-    for (int f = 0; f < 3; f++)
+    private int[,] mat;
+
+    public void Ingresar()
     {
-        for (int c = 0; c < 4; c++)
+        mat = new int[3, 4];
+        for (int f = 0; f < 3; f++)
         {
-            Console.Write("Ingrese componente:");
-            string linea;
-            linea = Console.ReadLine();
-            Mat[f, c] = int.Parse(linea);
+            for (int c = 0; c < 4; c++)
+            {
+                Console.Write("Ingrese componente:");
+                string linea = Console.ReadLine();
+                mat[f, c] = int.Parse(linea);
+            }
         }
     }
-}
-public void Imprimir()
-{
-    for (int f = 0; f < 3; f++)
+
+    public void Imprimir()
     {
-        for (int c = 0; c < 4; c++)
+        for (int f = 0; f < 3; f++)
         {
-            Console.Write(Mat[f, c] + " ");
+            for (int c = 0; c < 4; c++)
+            {
+                Console.Write(mat[f, c] + " ");
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
+        Console.ReadKey();
     }
-    Console.ReadKey();
 }
 
-static void Main(string[] args)
+class Program
 {
-    Matriz ma = new ();
-    ma.Ingresar();
-    ma.Imprimir();
+    static void Main(string[] args)
+    {
+        Matriz ma = new Matriz();
+        ma.Ingresar();
+        ma.Imprimir();
+    }
 }
