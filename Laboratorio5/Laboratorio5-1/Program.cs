2d3827122f59ba0ca3e0944349f236e3e9b28d2b
Angel Martinez <angel.martinez3@utp.ac.pa>
2024-09-11 19:13:20 -0500

    Laboratorio5 Completado

---
+++ Laboratorio5/Laboratorio5-1/Program.cs
@@ -0,0 +1,36 @@
+﻿private int[,] mat;
+
+public void Ingresar()
+
+{
+    Mat = new int[3, 4];
+    for (int f = 0; f < 3; f++)
+    {
+        for (int c = 0; c < 4; c++)
+        {
+            Console.Write("Ingrese componente:");
+            string linea;
+            linea = Console.ReadLine();
+            Mat[f, c] = int.Parse(linea);
+        }
+    }
+}
+public void Imprimir()
+{
+    for (int f = 0; f < 3; f++)
+    {
+        for (int c = 0; c < 4; c++)
+        {
+            Console.Write(Mat[f, c] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.ReadKey();
+}
+
+static void Main(string[] args)
+{
+    Matriz ma = new ();
+    ma.Ingresar();
+    ma.Imprimir();
+}
