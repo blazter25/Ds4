07511b3e849fe24d14b88f9c8106ea2771ac69af
Angel Martinez <angel.martinez3@utp.ac.pa>
2024-10-05 16:26:37 -0500

    laboratorio 11 completado

---
+++ Laboratorio 11/Laboratorio 11/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Laboratorio_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clickThisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clickThisToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 28);
+            // 
+            // clickThisToolStripMenuItem
+            // 
+            this.clickThisToolStripMenuItem.Name = "clickThisToolStripMenuItem";
+            this.clickThisToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
+            this.clickThisToolStripMenuItem.Text = "Click this";
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(194, 97);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(120, 73);
+            this.btnClickThis.TabIndex = 2;
+            this.btnClickThis.Text = "Click this";
+            this.btnClickThis.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.lblHelloWorld_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(211, 192);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(89, 16);
+            this.lblHelloWorld.TabIndex = 3;
+            this.lblHelloWorld.Text = "lblHelloWorld";
+            this.lblHelloWorld.Click += new System.EventHandler(this.lblHelloWorld_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 306);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clickThisToolStripMenuItem;
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
---
+++ Parcial#1/Parcial#1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedimos al usuario que ingrese el tamaño de la matriz (N)
+        Console.Write("Ingrese el tamaño de la matriz (N, debe ser par): ");
+        int N = int.Parse(Console.ReadLine());
+
+        // Validar que N sea par (si no es par, muestra un mensaje y termina el programa)
+        if (N % 2 != 0)
+        {
+            Console.WriteLine("N debe ser un número par.");
+            return;
+        }
+
+        // Creamos la matriz de tamaño N
+        int[,] matriz = new int[N, N];
+
+        // Ponemos los numero aleatoriamente en la diagonal excepto en el 1 y el ultimo
+        Random random = new Random();
+        for (int i = 0; i < N; i++)
+        {
+            if (i == 0 || i == N - 1)
+            {
+                matriz[i, i] = 0;
+            }
+            else
+            {
+                matriz[i, i] = random.Next(101, 201);
+            }
+        }
+
+        // Mostrar la matriz principal
+        Console.WriteLine("Matriz creada:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Aqui realizamos la suma de los valores de la diagonal de la matriz
+        int sumaDiagonal = 0;
+        for (int i = 0; i < N; i++)
+        {
+            sumaDiagonal += matriz[i, i];
+        }
+
+        // Mostrar la suma de la diagonal
+        Console.WriteLine("Suma de los valores de la diagonal de la matriz: " + sumaDiagonal);
+    }
+}
